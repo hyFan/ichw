b9f88b1b88732973be73dd174a8ba77d610af481
hyFan <501180426@qq.com>
2017-11-19 11:57:50 +0800

    调整参数

--- pyassign1/planets.py
+++ pyassign1/planets.py
@@ -1,11 +1,25 @@
+#!/usr/bin/env python3
+"""planets.py: Simulation of the orbits of six planets in solar system.
+
+
+__author__ = "Fan Hanyun"
+__pkuid__ = "1700017838"
+__email__ = "501180426@qq.com"
+"""
+
+
 import turtle
 import math
+
+
 wn = turtle.Screen()
+wn.bgcolor("black")
 sun = turtle.Turtle()
-sun.dot(10,"yellow")
+sun.dot(30,"yellow")
 sun.hideturtle()
 me = turtle.Turtle()
 me.shape("circle")
+me.turtlesize(0.5)
 me.color("blue")
 me.pencolor("blue")
 me.penup()
@@ -13,6 +27,7 @@ me.goto(40,0)
 me.pendown()
 ve = turtle.Turtle()
 ve.shape("circle")
+ve.turtlesize(0.5)
 ve.color("green")
 ve.pencolor("green")
 ve.penup()
@@ -20,13 +35,15 @@ ve.goto(60,0)
 ve.pendown()
 ea = turtle.Turtle()
 ea.shape("circle")
-ea.color("black")
-ea.pencolor("black")
+ea.turtlesize(0.5)
+ea.color("white")
+ea.pencolor("white")
 ea.penup()
 ea.goto(80,0)
 ea.pendown()
 ma = turtle.Turtle()
 ma.shape("circle")
+ma.turtlesize(0.5)
 ma.color("red")
 ma.pencolor("red")
 ma.penup()
@@ -34,34 +51,43 @@ ma.goto(90,0)
 ma.pendown()
 ju = turtle.Turtle()
 ju.shape("circle")
+ju.turtlesize(0.7)
 ju.color("brown")
 ju.pencolor("brown")
 ju.penup()
-ju.goto(150,0)
+ju.goto(170,0)
 ju.pendown()
 sa = turtle.Turtle()
 sa.shape("circle")
+sa.turtlesize(0.7)
 sa.color("lightblue")
 sa.pencolor("lightblue")
 sa.penup()
-sa.goto(180,0)
+sa.goto(200,0)
 sa.pendown()
-for t in range(1000):
-        y = 40*math.sin(math.radians(t))
-        x = 40*math.cos(math.radians(t))
-        me.goto(x,y)
-        y = 50*math.sin(math.radians(t))
-        x = 55*math.cos(math.radians(t))+5
-        ve.goto(x,y)
-        y = 90*math.sin(math.radians(t))+5
-        x = 80*math.cos(math.radians(t))
-        ea.goto(x,y)
-        y = 120*math.sin(math.radians(t))+10
-        x = 90*math.cos(math.radians(t))
-        ma.goto(x,y)
-        y = 140*math.sin(math.radians(t))
-        x = 150*math.cos(math.radians(t))
-        ju.goto(x,y)
-        y = 160*math.sin(math.radians(t))
-        x = 175*math.cos(math.radians(t))+5
-        sa.goto(x,y)
+def planets():
+        for t in range(1000):
+                y = 40*math.sin(math.radians(4.5*t+5))
+                x = 40*math.cos(math.radians(4.5*t+5))
+                me.goto(x,y)
+                y = 50*math.sin(math.radians(4*t))
+                x = 55*math.cos(math.radians(4*t))+5
+                ve.goto(x,y)
+                y = 90*math.sin(math.radians(3*t+10))+5
+                x = 80*math.cos(math.radians(3*t+10))
+                ea.goto(x,y)
+                y = 110*math.sin(math.radians(2.5*t))+10
+                x = 90*math.cos(math.radians(2.5*t))
+                ma.goto(x,y)
+                y = 150*math.sin(math.radians(2*t+5))
+                x = 160*math.cos(math.radians(2*t+5))+10
+                ju.goto(x,y)
+                y = 180*math.sin(math.radians(t))
+                x = 195*math.cos(math.radians(t))+5
+                sa.goto(x,y)
+def main():
+        planets()
+
+if __name__ == '__main__':
+        main()
+        
